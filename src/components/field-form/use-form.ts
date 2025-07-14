10ce5feac0abc59c07d54bb807ce370c8a6f08b6
mannies <nanthipathp@set.or.th>
2025-07-14 21:05:38 +0700

    enhance: useform

---
+++ src/components/field-form/use-form.ts
@@ -0,0 +1,39 @@
+import { useMemo } from 'react'
+import { useForm, useFormState } from 'react-final-form'
+
+export const useCustomForm = <T>() => {
+  const form = useForm()
+  const { change, submit } = useForm<any>()
+  const { getFieldState, reset } = useForm<T>()
+  const formState = useFormState<T>({
+    subscription: {
+      initialValues: true,
+      values: true,
+      errors: true,
+      submitFailed: true,
+      submitting: true,
+      touched: true,
+    },
+  })
+
+  const memoizedValues = useMemo(() => formState.values, [formState.values])
+  const memoizedErrors = useMemo(() => formState.errors, [formState.errors])
+  const memoizedTouched = useMemo(() => formState.touched, [formState.touched])
+
+  return {
+    change,
+    submit,
+    form,
+    formState,
+    getFieldState,
+    reset,
+    values: memoizedValues,
+    errors: memoizedErrors,
+    submitFailed: formState.submitFailed,
+    submitting: formState.submitting,
+    touched: memoizedTouched,
+  }
+}
+
+
+
